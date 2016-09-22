bb7a227f06beca9ea8334804f09c1f110d319546
diananr <navarroruiz.diana>
2016-09-22 13:05:53 -0500

    añadiendo version 0.2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 window.addEventListener("load", function() {
 	var cajaLista = document.getElementById("contenedorLista");
 	var formuLista = document.getElementById("formLista");
+	var nombreLista = document.getElementById("nombreLista");
+	var botonGuardar = document.getElementById("btnGuardar");
+	var contenLista = document.getElementById("contenedor");
 
 	cajaLista.addEventListener("click", function(){
-  		formuLista.style.display = "block";
   		cajaLista.style.display = "none";
+  		formuLista.style.display = "block";
 	});
+
+	botonGuardar.addEventListener("click", function(){
+		formuLista.style.display = "none";
+		agregarLista(nombreLista);
+	});
+
+	function agregarLista(nombre){
+		var nuevaLista = document.createElement("div");
+		nuevaLista.innerText = nombre.value;
+		contenLista.insertBefore(nuevaLista, contenLista.childNodes[0]);
+		nuevaLista.classList.add("newList");
+
+		var tarjeta = document.createElement("div");
+		tarjeta.innerText = "Añadir una tarjeta..."
+		contenLista.insertBefore(tarjeta, contenLista.childNodes[1]);
+		tarjeta.classList.add("tarjeta");
+	}
 });
