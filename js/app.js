7390c5a70bf897068d4fc29e780250478282bd4a
diananr <navarroruiz.diana>
2016-10-07 14:50:36 -0500

    agregando boton cerrar

--- js/app.js
+++ js/app.js
@@ -7,20 +7,29 @@
 	var formuLista = document.getElementById("formLista");
 	var nombreLista = document.getElementById("nombreLista");
 	var botonGuardar = document.getElementById("btnGuardar");
+	var botonCerrar = document.getElementById("btnCerrar");
 	var conta = 1;
 
 	function cargaPagina(){
 		cajaLista.addEventListener("click", apareceFormulario);
 	}
 	function apareceFormulario(){
-		cajaLista.style.display = "none";
-		formuLista.style.display = "block";
+		cajaLista.classList.add("desaparece");
+		formuLista.classList.remove("desaparece");
+		nombreLista.value = "";
 		nombreLista.focus();
-	    contenLista.classList.add("p-7");
-	    botonGuardar.addEventListener("click", agregar);
+		contenLista.classList.add("p-7","estiloLista");
+
+		botonCerrar.addEventListener("click", cerrar);
+		botonGuardar.addEventListener("click", agregar);
 	}
-	function agregar(){
-		formuLista.style.display = "none";
+	function cerrar(){
+		formuLista.classList.add("desaparece");
+		cajaLista.classList.remove("desaparece");
+	}
+	function agregar(e){
+		e.preventDefault();
+		formuLista.classList.add("desaparece");
 		agregarLista(nombreLista, this);
 		agregaContenedor();
 		nombreLista.value = "";
@@ -31,68 +40,79 @@
 		var nuevaLista = document.createElement("div");
 
 		nuevaLista.innerText = nombre.value;
-		padre.insertBefore(nuevaLista, padre.childNodes[0]);
 		nuevaLista.classList.add("nomList");
+		padre.insertBefore(nuevaLista, padre.childNodes[0]);
 
 		tarjeta.innerText = "Añadir una tarjeta...";
-		padre.appendChild(tarjeta);
 		tarjeta.classList.add("tarjeta");
+		padre.appendChild(tarjeta);
 
 		tarjeta.addEventListener("click", apareceFormuTarjeta);
 		padre.addEventListener("dragover", arrastraSobre);
 		padre.addEventListener("drop", soltar);
 	}
 	function agregaContenedor(){
-		var nuevoContenedor = document.createElement("div");
-		contenTodo.appendChild(nuevoContenedor);
+		cajaLista.classList.remove("desaparece");
 
+		var nuevoContenedor = document.createElement("div");
+		nuevoContenedor.classList.add("nuevoConten");
+		nuevoContenedor.classList.add("p-7", "estiloLista");
+		
 		nuevoContenedor.insertBefore(cajaLista, nuevoContenedor.childNodes[0]);
 		nuevoContenedor.insertBefore(formuLista, nuevoContenedor.childNodes[0]);
-
-		nuevoContenedor.classList.add("nuevoConten");
-		nuevoContenedor.classList.add("p-7");
-
-		cajaLista.style.display = "block";
+		contenTodo.appendChild(nuevoContenedor);
 	}
 	function apareceFormuTarjeta(){
-		this.style.display = "none";
+		this.classList.add("desaparece");
 		agregarTarjeta(this.parentElement);
 	}
 	function agregarTarjeta(padre){
 		var nuevaTarjeta = document.createElement("form");
 		var rectangulo = document.createElement("textarea");
 		var btnAnadir = document.createElement("button");
-
-		nuevaTarjeta.insertBefore(rectangulo, nuevaTarjeta.childNodes[0]);
-		nuevaTarjeta.insertBefore(btnAnadir, nuevaTarjeta.childNodes[1]);
-		padre.appendChild(nuevaTarjeta);
-
-		btnAnadir.type = "button";
-		btnAnadir.innerText = "Añadir";
+		var btnClose = document.createElement("button");
 
 		nuevaTarjeta.classList.add("ntarjeta");
 		rectangulo.classList.add("ntextarea");
-		btnAnadir.classList.add("nboton");
+		btnAnadir.classList.add("boton");
+		btnClose.classList.add("boton", "cerrar");
 
+		btnAnadir.type = "submit";
+		btnAnadir.innerText = "Añadir";
+		btnClose.type = "button";
+		btnClose.innerText = "X";
+
+		nuevaTarjeta.insertBefore(rectangulo, nuevaTarjeta.childNodes[0]);
+		nuevaTarjeta.insertBefore(btnAnadir, nuevaTarjeta.childNodes[1]);
+		nuevaTarjeta.insertBefore(btnClose, nuevaTarjeta.childNodes[2]);
+		padre.appendChild(nuevaTarjeta);
+		
 		rectangulo.focus();
 
 		btnAnadir.addEventListener("click", anade)
+		btnClose.addEventListener("click", noAnade);
 	}
-	function anade(){
+	function anade(e){
+		e.preventDefault();
+
 		guardarTarjeta(this.parentElement.parentElement, this.previousSibling);
+		this.parentElement.remove();
+	}
+	function noAnade(){
+		this.parentElement.previousSibling.classList.remove("desaparece");
 		this.parentElement.remove();
 	}
 	function guardarTarjeta(padre,rectangulo){
 		var nombreTarjeta = document.createElement("div");
-		nombreTarjeta.innerText = rectangulo.value;
-		padre.insertBefore(nombreTarjeta, padre.lastChild);
-
 		nombreTarjeta.classList.add("cadaTarjeta");
-		padre.appendChild(nombreTarjeta.previousSibling);
-		padre.lastChild.style.display = "block";
-
+		nombreTarjeta.innerText = rectangulo.value;
 		nombreTarjeta.setAttribute("draggable", "true");
 		nombreTarjeta.setAttribute("id","tarjeta"+conta);
+		
+		padre.insertBefore(nombreTarjeta, padre.lastChild);
+		padre.appendChild(nombreTarjeta.previousSibling);
+
+		padre.lastChild.classList.remove("desaparece");
 
 		nombreTarjeta.addEventListener("dragstart", empiezaArrastrar);
 		nombreTarjeta.addEventListener("dragend", terminaArrastrar);
@@ -102,6 +122,10 @@
 	function empiezaArrastrar(e){
 	 	e.dataTransfer.setData("text", this.id);
 	 	this.classList.add("estiloArrastrando");
+	 	var estilos = document.getElementsByClassName("estiloLista");
+	 	for(var i = 0; i < estilos.length; i++){
+	 		estilos[i].classList.remove("animated", "bounce", "swing");
+	 	}
 	}
 	function arrastraSobre(e){
 	  	e.preventDefault();
